20bf70a5e0f7f50512d08de03321f71c5973422f
Edwin Jose Palathinkal <edwinhere@gmail.com>
2015-07-17 17:05:14 +0800

    Added support for PGP encrypted blobs.

--- src/main/java/com/enhide/services/EmailService.java
+++ src/main/java/com/enhide/services/EmailService.java
@@ -5,10 +5,29 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 import com.sun.jersey.api.client.filter.HTTPBasicAuthFilter;
 import com.sun.jersey.multipart.FormDataMultiPart;
-import com.sun.jersey.multipart.file.FileDataBodyPart;
-import java.io.File;
+import com.sun.jersey.multipart.file.StreamDataBodyPart;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import javax.ws.rs.core.MediaType;
+import org.apache.james.mime4j.dom.Header;
+import org.apache.james.mime4j.dom.Message;
+import org.apache.james.mime4j.dom.MessageBuilder;
+import org.apache.james.mime4j.dom.MessageWriter;
+import org.apache.james.mime4j.dom.Multipart;
+import org.apache.james.mime4j.dom.SingleBody;
+import org.apache.james.mime4j.dom.address.Mailbox;
+import org.apache.james.mime4j.field.Fields;
+import org.apache.james.mime4j.field.address.AddressBuilder;
+import org.apache.james.mime4j.field.address.ParseException;
+import org.apache.james.mime4j.message.BodyPart;
+import org.apache.james.mime4j.message.DefaultMessageBuilder;
+import org.apache.james.mime4j.message.DefaultMessageWriter;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
@@ -19,35 +38,106 @@ import org.springframework.stereotype.Service;
 @Service
 public class EmailService {
 
-  @Value(value = "${mailgun.key}")
-  private String key;
+	@Value(value = "${mailgun.key}")
+	private String key;
 
-  @Value(value = "${mailgun.resource}")
-  private String resource;
+	@Value(value = "${mailgun.resource}")
+	private String resource;
 
-  public ClientResponse sendMimeMessage(
-          String from,
-          List<String> tos,
-          List<String> ccs,
-          List<String> bccs) {
-    Client client = Client.create();
-    client.addFilter(new HTTPBasicAuthFilter("api", key));
-    WebResource webResource = client.resource(resource);
-    FormDataMultiPart form = new FormDataMultiPart();
-    form.field("from", from);
-    for (String to : tos) {
-      form.field("to", to);
-    }
-    for (String cc : ccs) {
-      form.field("cc", cc);
-    }
-    for (String bcc : bccs) {
-      form.field("bcc", bcc);
-    }
-    File mimeFile = new File("message.mime");
-    form.bodyPart(new FileDataBodyPart("message", mimeFile,
-            MediaType.APPLICATION_OCTET_STREAM_TYPE));
-    return webResource.type(MediaType.MULTIPART_FORM_DATA_TYPE).
-            post(ClientResponse.class, form);
-  }
+	public ClientResponse sendMimeMessage(
+		String from,
+		List<String> tos,
+		List<String> ccs,
+		List<String> bccs,
+		String subject,
+		String pgp
+	) throws IOException, ParseException {
+		Client client = Client.create();
+		client.addFilter(new HTTPBasicAuthFilter("api", key));
+		WebResource webResource = client.resource(resource);
+		FormDataMultiPart form = new FormDataMultiPart();
+		InputStream inputStream = encrypt(from, tos, ccs, bccs, subject, pgp);
+		for (String to : tos) {
+			form.field("to", to);
+		}
+		form.bodyPart(new StreamDataBodyPart("message", inputStream));
+		return webResource
+			.type(MediaType.MULTIPART_FORM_DATA_TYPE)
+			.post(ClientResponse.class, form);
+	}
+
+	private InputStream encrypt(
+		String from,
+		List<String> tos,
+		List<String> ccs,
+		List<String> bccs,
+		final String subject,
+		final String pgp
+	) throws IOException, ParseException {
+		MessageBuilder builder = new DefaultMessageBuilder();
+		Message message = builder.newMessage();
+		{
+			Header header = builder.newHeader();
+			{
+				AddressBuilder addressBuilder = AddressBuilder.DEFAULT;
+				header.addField(Fields.from(addressBuilder.parseMailbox(from)));
+				for (String to : tos) {
+					header.addField(Fields.to(addressBuilder.parseAddress(to)));
+				}
+				for (String cc : ccs) {
+					header.addField(Fields.cc(addressBuilder.parseAddress(cc)));
+				}
+				for (String bcc : bccs) {
+					header.addField(Fields.bcc(addressBuilder.parseAddress(bcc)));
+				}
+				header.addField(Fields.subject(subject));
+				Map<String, String> map = new HashMap<>();
+				map.put("protocol", "application/pgp-encrypted");
+				map.put("boundary", "lICdWpALSxNuvFoHT9h2kd6OuMwufkLpT");
+				header.addField(Fields.contentType("multipart/encrypted", map));
+				message.setHeader(header);
+			}
+			Multipart multipart = builder.newMultipart("");
+			{
+				multipart.setPreamble("This is an OpenPGP/MIME encrypted message (RFC 4880 and 3156)");
+				BodyPart version = new BodyPart();
+				{
+					Header versionHeader = builder.newHeader();
+					versionHeader.addField(Fields.contentType("application/pgp-encrypted"));
+					version.setHeader(versionHeader);
+					version.setBody(new SingleBody() {
+						@Override
+						public InputStream getInputStream() throws IOException {
+							return new ByteArrayInputStream("Version: 1".getBytes(StandardCharsets.US_ASCII));
+						}
+					});
+					multipart.addBodyPart(version);
+				}
+				BodyPart octets = new BodyPart();
+				{
+					Header octetHeader = builder.newHeader();
+					Map<String, String> octetMap = new HashMap<>();
+					octetMap.put("name", "encrypted.asc");
+					octetHeader.addField(Fields.contentType("application/octet-stream", octetMap));
+					Map<String, String> dispositionMap = new HashMap<>();
+					dispositionMap.put("filename", "encrypted.asc");
+					octetHeader.addField(Fields.contentDisposition("inline", octetMap));
+					octets.setHeader(octetHeader);
+					octets.setBody(new SingleBody() {
+						@Override
+						public InputStream getInputStream() throws IOException {
+							return new ByteArrayInputStream(pgp.getBytes(StandardCharsets.US_ASCII));
+						}
+					});
+					multipart.addBodyPart(octets);
+				}
+			}
+			multipart.setParent(message);
+			message.setBody(multipart);
+			MessageWriter writer = new DefaultMessageWriter();
+			ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+			writer.writeMessage(message, outputStream);
+			return new ByteArrayInputStream(outputStream.toByteArray());
+		}
+	}
 }
--- src/test/java/com/enhide/services/EmailServiceTest.java
+++ src/test/java/com/enhide/services/EmailServiceTest.java
@@ -2,7 +2,9 @@ package com.enhide.services;
 
 import com.enhide.Main;
 import com.sun.jersey.api.client.ClientResponse;
+import java.io.IOException;
 import java.util.Arrays;
+import org.apache.james.mime4j.field.address.ParseException;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -20,17 +22,42 @@ import org.springframework.util.Assert;
 @SpringApplicationConfiguration(classes = Main.class)
 public class EmailServiceTest {
 
-  @Autowired
-  private EmailService emailService;
+	@Autowired
+	private EmailService emailService;
 
-  @Test
-  public void testSendMimeMessage() {
-    ClientResponse sendMimeMessage = emailService.sendMimeMessage(
-            "Excited User <mailgun@sandboxe61c04d2f5d7416c9137dc01dc3fd3b4.mailgun.org>",
-            Arrays.asList("edwinhere@gmail.com"),
-            Arrays.asList(new String[]{}),
-            Arrays.asList(new String[]{})
-    );
-    Assert.isTrue(sendMimeMessage.getStatus() == 200);
-  }
+	@Test
+	public void testSendMimeMessage() throws IOException, ParseException {
+		String pgp = "-----BEGIN PGP MESSAGE-----\n"
+			+ "Version: GnuPG v2\n"
+			+ "\n"
+			+ "hQIMA6whYqiToljLARAAhSOp1Ni7rqujc3OV/v0iJmIcGK53iqp78XOzp16xDaJD\n"
+			+ "A/WRLbJIXmUOqiKoAqEWxbWyJzPRuLOK6InEzt8vBknO01pRZS+YiYNd+i4+T7Uw\n"
+			+ "FWHsGJL8wHmEo0yz2ZGBpnmiuy7vY7tCXi8ccavlbfqsr6KiBylVpMZcOxVpTq3m\n"
+			+ "BX3crObgb5wvWD5qmsTD+7ofVVld2EzPIQc/ax8imSMuu1S3RVwdiYBdCMUtlvQ1\n"
+			+ "XqfMM9H7oW1pUdW2qqh5NnxkSUhp3T3Vb38eXSXcJngcQCZrFDIQPECqRIl3LzHM\n"
+			+ "vTKNo8b5zKMLPHXngL+DxBfIcVWSl5m2ei0QNE3K05HcaeZQ9vNyTn5Ic+bd0sHt\n"
+			+ "YRUtRDFcnL8+DUzAHANcP8SpYIQdVsiPrKmbtMG/Uzna7hnR4zxGseMk7GTEBszf\n"
+			+ "I2e3hJ4SB3xeQU9Pv/Ar6dtZPGIemG9El3/jpovbjKSa8XqOsZSmp+R99e8znh48\n"
+			+ "Kl+ndW1/LmSu4XN07+0KopOq8e6A3jEJXjx1uNcBMjWO7D4PXYTqcrFtNZcDbX1z\n"
+			+ "dn5b1aO5CQEcKaE/3znigTefM0saFWFEa9P0mKKwqQWWOOe8900AW7BxX4P/0t2c\n"
+			+ "vXkonmZjlm3RQA0bLfLOIJnY16F+kVSiMHl+kOe5TwLVRhP4Or9TbaNEv06IGJfS\n"
+			+ "wEQB5FdwKRl4vE1KDh3VRqQODoM0wGKXhCG0FmOFRIUFrwgL8FQOXGX28dIqjLtj\n"
+			+ "VjKahnxOw3fgdo+ln+OfZfj6nxmJ9TnpW2J+bWeoyWzjtRa9yVTAQe2HFYtM/JDS\n"
+			+ "Z1CDDMU32sG7jMWyyaJGuteXKLlJcugohvxY7Z9OtkRTGcmoa50tNlxA2ogi/S+/\n"
+			+ "UfwaHat4I9J5bHqwwJ3jjW3W4I/EwHKyq7EsMKN9aKQ+nn+moxO3IzHzuBpmu5qG\n"
+			+ "tlfqIxMuPTEQmnSgHjm7SPaqqD8OC+14nkRAmwcLWOI4HjZlIVvRT/j1dcEimRRd\n"
+			+ "PWImUmpX9UrTzfjtJ0kj6ug6q/Pe7w==\n"
+			+ "=fssi\n"
+			+ "-----END PGP MESSAGE-----\n";
+
+		ClientResponse sendMimeMessage = emailService.sendMimeMessage(
+			"Excited User <mailgun@sandboxe61c04d2f5d7416c9137dc01dc3fd3b4.mailgun.org>",
+			Arrays.asList("edwinhere@gmail.com"),
+			Arrays.asList(new String[]{}),
+			Arrays.asList(new String[]{}),
+			"Hello",
+			pgp
+		);
+		Assert.isTrue(sendMimeMessage.getStatus() == 200);
+	}
 }
